d45ac2ce1e9978ecfdbf2cd51e9d43de78a9c052
Akash T Nair <akashnair316@gmail.com>
2025-08-10 10:27:49 +0530

    - Introducing C files mapping from assembly file. - In this commit, added a C file with some void functions and without   any library just to showcase how in assembly branching is done. - This commit's code will get compiled but due to wrong stack pointer   handling in linker script in will stop execution and through and exception to CPU and it will halt.
    Signed-off-by: Akash T Nair <akashnair316@gmail.com>

---
+++ FreeRTOS-ARM-BOOT/main.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+
+void caller_test_func(void)
+{
+    //printf("var value after module : %d\r\n", var);
+    return;
+}
+
+void main(void)
+{
+    caller_test_func();
+    return;
+}
